60026f3de83017abc587d2a312131cb8525d3dc0
tgrays <tgrays2001@gmail.com>
2014-04-30 21:33:13 -0600

    2nd day changes
    Trying to get the logo animated and to show correct in the background.
    Need to look into more of a contemporary look

--- Generic.js
+++ Generic.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
-    $('nav li').mouseenter(function() {
+    $('nav li').mouseenter(function () {
 			$(this).children('.submenu').slideToggle('fast');
+			 /*$('nav li').mouseleave(function() {
+			  $(this).children('.submenu').slideToggle('slow'); 
+			 });*/
 		});
 });
+var interval;
+    var rotation = 5;
+    var rotObj;
+    
+    function doRotation() {
+        rotObj.rotate(rotation);
+        rotation *= -1;
+    }
+    
+$('.logo img').mouseenter(function() {
+rotation = 3;
+        rotObj = $(this);
+        interval = setInterval(doRotation, 100);
+});
+    
+    $('.logo img').mouseleave(function() {
+        clearInterval(interval);
+    });
